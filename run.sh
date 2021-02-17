6bf6d6816dba1da279d7100c558305922d239101
ghjardim <ghjardim@gmail.com>
2021-02-17 13:06:40 -0300

    Visual changes, attempt to fix bugs

--- run.sh
+++ run.sh
@@ -2,64 +2,67 @@
 
 echo ""
 echo "------------------------------------"
-echo " UPDATING YOUR APT CACHE..."
+echo " (1/8) UPDATING YOUR APT CACHE..."
 echo "------------------------------------"
 sudo apt update
 
 echo ""
 echo "------------------------------------"
-echo " INSTALLING THE PACKAGES..."
+echo " (2/8) INSTALLING THE PACKAGES..."
 echo "------------------------------------"
 cat packages.txt | grep -v "#" | xargs sudo apt install -y
+sudo apt update
+sudo update-command-not-found
 
 echo ""
 echo " All packages now installed."
 
 echo ""
 echo "------------------------------------"
-echo " CLONING DOTFILES..."
+echo " (3/8) CLONING DOTFILES..."
 echo "------------------------------------"
 yadm clone "https://github.com/ghjardim/dotfiles"
 cd
 
 echo ""
 echo "------------------------------------"
-echo " GETTING FONTS..."
+echo " (4/8) GETTING FONTS..."
 echo "------------------------------------"
 echo " [i] These fonts does not have Debian packages associated."
 echo ""
-mkdir ~/.fonts/
+mkdir $HOME/.fonts/
 mkdir /tmp/fonts/
 cd /tmp/fonts/
 wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip"
 unzip UbuntuMono.zip
-mv "Ubuntu Mono Nerd Font Complete Mono.ttf" ~/.fonts/
+mv "Ubuntu Mono Nerd Font Complete Mono.ttf" $HOME/.fonts/
 cd
 
 echo ""
 echo "------------------------------------"
-echo " INSTALLING OH-MY-ZSH..."
+echo " (5/8) INSTALLING OH-MY-ZSH..."
 echo "------------------------------------"
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cd
 
 echo ""
 echo "------------------------------------"
-echo " BUILDING ALACRITTY..."
+echo " (6/8) DOWNLOADING ALACRITTY..."
 echo "------------------------------------"
 mkdir $HOME/Applications/
 cd $HOME/Applications/
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-apt-get install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev python3
 cargo build --release
+	# Error: cargo not found
 mv ./target/release/alacritty ~/.local/bin/
 cd
 
 echo ""
 echo "------------------------------------"
-echo " BUILDING I3-GAPS..."
+echo " (7/8) BUILDING I3-GAPS..."
 echo "------------------------------------"
 cd $HOME/Applications/
 git clone "https://github.com/maestrogerardo/i3-gaps-deb"
@@ -69,7 +72,7 @@ cd
 
 echo ""
 echo "------------------------------------"
-echo " BUILDING QUTEBROWSER..."
+echo " (8/8) BUILDING QUTEBROWSER..."
 echo "------------------------------------"
 echo " [i] This is for you to get latest, more secure version of Qutebrowser"
 echo "     Only the latest version is compatible with my dotfiles."
@@ -78,3 +81,4 @@ cd $HOME/Applications/
 git clone "https://github.com/qutebrowser/qutebrowser.git"
 cd qutebrowser
 python3 scripts/mkvenv.py
+# Failed building
