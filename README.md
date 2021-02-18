69abfdfe868b1f6ce8b217a590d665c3464a1b89
ghjardim <ghjardim@gmail.com>
2021-02-18 10:53:54 -0300

    Change commands order, add 'press enter to continue...'

--- README.md
+++ README.md
@@ -3,6 +3,6 @@ Script to automatically install packages after a minimal Debian installation.
 
 Meant to work with my [dotfiles](https://github.com/ghjardim/dotfiles).
 
-It automatically install all packages, build the packages needed, install extra fonts and clone my dotfiles to a new, fresh-installed, debian-based GNU/Linux distribution.
+It automatically installs all packages, build the packages needed, install extra fonts and clone my dotfiles to a new, fresh-installed, debian-based GNU/Linux distribution.
 
 **_PROCEED WITH CAUTION_: This repo script is NOT TESTED yet.**
--- run.sh
+++ run.sh
@@ -2,14 +2,21 @@
 
 echo ""
 echo "------------------------------------"
-echo " (1/9) UPDATING YOUR APT CACHE..."
+echo " (1/8) UPDATING YOUR APT CACHE..."
 echo "------------------------------------"
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
 sudo apt update
+
+
 
 echo ""
 echo "------------------------------------"
-echo " (2/9) INSTALLING THE PACKAGES..."
+echo " (2/8) INSTALLING THE PACKAGES..."
 echo "------------------------------------"
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
 cat packages.txt | grep -v "#" | xargs sudo apt install -y
 sudo apt update
 sudo update-command-not-found
@@ -17,19 +24,29 @@ sudo update-command-not-found
 echo ""
 echo " All packages now installed."
 
-echo ""
-echo "------------------------------------"
-echo " (3/9) CLONING DOTFILES..."
-echo "------------------------------------"
-yadm clone "https://github.com/ghjardim/dotfiles"
-cd
+
 
 echo ""
 echo "------------------------------------"
-echo " (4/9) GETTING FONTS..."
+echo " (3/8) CLONING DOTFILES..."
+echo "------------------------------------"
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
+yadm clone "https://github.com/ghjardim/dotfiles"
+cd
+
+
+
+echo ""
+echo "------------------------------------"
+echo " (4/8) GETTING FONTS..."
 echo "------------------------------------"
 echo " [i] These fonts does not have Debian packages associated."
 echo ""
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
 mkdir $HOME/.fonts/
 mkdir /tmp/fonts/
 cd /tmp/fonts/
@@ -38,17 +55,16 @@ unzip UbuntuMono.zip
 mv "Ubuntu Mono Nerd Font Complete Mono.ttf" $HOME/.fonts/
 cd
 
-echo ""
-echo "------------------------------------"
-echo " (5/9) INSTALLING OH-MY-ZSH..."
-echo "------------------------------------"
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cd
+
 
 echo ""
 echo "------------------------------------"
-echo " (6/9) DOWNLOADING ALACRITTY..."
+echo " (5/8) DOWNLOADING AND COMPILING"
+echo "       ALACRITTY..."
 echo "------------------------------------"
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
 mkdir $HOME/Applications/
 cd $HOME/Applications/
 git clone https://github.com/alacritty/alacritty.git
@@ -60,22 +76,33 @@ cargo build --release
 mv ./target/release/alacritty ~/.local/bin/
 cd
 
+
+
 echo ""
 echo "------------------------------------"
-echo " (7/9) BUILDING I3-GAPS..."
+echo " (6/8) BUILDING I3-GAPS..."
 echo "------------------------------------"
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
 cd $HOME/Applications/
 git clone "https://github.com/maestrogerardo/i3-gaps-deb"
 cd i3-gaps-deb
 ./i3-gaps-deb
 cd
 
+
+
+
 echo ""
 echo "------------------------------------"
-echo " (8/9) BUILDING QUTEBROWSER..."
+echo " (7/8) BUILDING QUTEBROWSER..."
 echo "------------------------------------"
 echo " [i] This is for you to get latest, more secure version of Qutebrowser"
 echo "     Only the latest version is compatible with my dotfiles."
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
 sudo apt install -y --no-install-recommends git ca-certificates python3 python3-venv asciidoc libglib2.0-0 libgl1 libfontconfig1 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0 libdbus-1-3 libyaml-dev gcc python3-dev libnss3
 cd $HOME/Applications/
 git clone "https://github.com/qutebrowser/qutebrowser.git"
@@ -84,11 +111,33 @@ python3 scripts/mkvenv.py
 # Failed building
 cd
 
+
+
 echo ""
 echo "------------------------------------"
-echo " (9/9) GETTING PFETCH..."
+echo " (8/8) INSTALLING OH-MY-ZSH..."
+echo " ...And its components..."
 echo "------------------------------------"
+echo " [i] IMPORTANT: EXIT ZSH when its installed!!!!"
+
+read -n 1 -s -r -p $'Press enter to continue...\n' key; while [ "$key" != '' ]; do read -n 1 -s -r -p $'Wrong key! Press enter to continue...\n' key; done
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cd
+echo "\n >>> Getting pfetch..."
 cd $HOME/.local/bin
 wget "https://raw.githubusercontent.com/dylanaraps/pfetch/master/pfetch"
 chmod +x pfetch
 cd
+echo ""
+echo "\n >>> Getting fast-highlight-syntax plugin..."
+git clone https://github.com/zdharma/fast-syntax-highlighting.git \
+  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+echo ""
+echo "\n >>> Getting zsh-autosuggestions..."
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+echo ""
+echo "------------------------------------"
+echo " DONE! REBOOT YOUR SYSTEM NOW..."
+echo "------------------------------------"
